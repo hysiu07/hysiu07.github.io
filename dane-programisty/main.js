4a7a8c3bc865d75cb957c533a760bfac710a3642
Daniel <hysiu07@gmail.com>
2023-02-11 20:18:38 +0100

    .

--- dane-programisty/main.js
+++ dane-programisty/main.js
@@ -1,35 +1,36 @@
-const info = document.querySelector('#dane-programisty');
-const btn = document.querySelector('.btn');
+// const info = document.querySelector('#dane-programisty');
+// const btn = document.querySelector('.btn');
 
-fetch(
-	'https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php'
-)
-	.then((res) => res.json())
-	.then((data) => {
-		console.log(data);
+// fetch(
+// 	'https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php'
+// )
+// 	.then((res) => res.json())
+// 	.then((data) => {
+// 		console.log(data);
 
-		const name = data.imie;
-		const lastName = data.nazwisko;
-		const occupation = data.zawod;
-		const company = data.firma;
+// 		const name = data.imie;
+// 		const lastName = data.nazwisko;
+// 		const occupation = data.zawod;
+// 		const company = data.firma;
 
-		const showUser = () => {
-			info.textContent = `IMIĘ: ${name}, NAZWISKO: ${lastName}, ZAWÓD: ${occupation}, FIRMA: ${company}`;
-		};
+// 		const showUser = () => {
+// 			info.textContent = `IMIĘ: ${name}, NAZWISKO: ${lastName}, ZAWÓD: ${occupation}, FIRMA: ${company}`;
+// 		};
 
-		btn.addEventListener('click', showUser);
-	});
+// 		btn.addEventListener('click', showUser);
+// 	});
 
-$(document).ready(function () {
-	$.getJSON(
-		'https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php',
-		function (data) {
-            console.log(data);
-			$('#btn').click(function () {
-				$('#dane-programisty-jquery').text(
-					`IMIĘ: ${data.imie}, NAZWISKO: ${data.nazwisko}, ZAWÓD: ${data.zawod}, FIRMA: ${data.firma}`
-				);
-			});
-		}
-	);
-});
+
+// $(document).ready(function () {
+// 	$.getJSON(
+// 		'https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php',
+// 		function (data) {
+//             console.log(data);
+// 			$('.btn_two').click(function () {
+// 				$('#dane-programisty-jquery').text(
+// 					`IMIĘ: ${data.imie}, NAZWISKO: ${data.nazwisko}, ZAWÓD: ${data.zawod}, FIRMA: ${data.firma}`
+// 				);
+// 			});
+// 		}
+// 	);
+// });
