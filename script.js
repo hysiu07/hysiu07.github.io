26641b72e0ed52fe96f25348731afb8b9df72bd3
Daniel <hysiu07@gmail.com>
2023-04-26 15:24:45 +0200

    added mobile nav-menu

--- script.js
+++ script.js
@@ -2,11 +2,26 @@
 
 const barsBtn = document.querySelector('.nav-container__bars-btn');
 const menu = document.querySelector('.nav-container__links');
+const navLinks = document.querySelectorAll('.nav-container__links li');
 
 const showMenu = () => {
+	const delay = 0.6;
+
 	menu.classList.toggle('show-menu');
-	console.log('dziala');
+	if (menu.classList.contains('show-menu')) {
+		barsBtn.style.borderColor = 'white';
+		barsBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+		navLinks.forEach((el) => {
+			el.style.animation = `moveLink ${delay}s ease-in 1 forwards`;
+		});
+	} else {
+		barsBtn.style.borderColor = '#b95cdd';
+		barsBtn.innerHTML =
+			' <div class="first-bar bar"></div><div class="second-bar bar"></div><div class="third-bar bar"></div>';
+		navLinks.forEach((el) => {
+			el.style.animation = '';
+		});
+	}
 };
 
 barsBtn.addEventListener('click', showMenu);
-
